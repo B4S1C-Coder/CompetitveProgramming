4eed22361f3167e2eaf181d507188bea68304e99
B4S1C-Coder <saksham2005mittal@gmail.com>
2025-04-25 04:10:03 +0000

    State update -- oops lab

--- oops_assgn/Lab7q10.cc
+++ oops_assgn/Lab7q10.cc
@@ -1,12 +1,18 @@
 #include <iostream>
 
 class Rectangular {
-
+public:
+  Rectangular(float r=0.0, float c=0.0) : mr(r), mc(c) {}
+  void show() { std::cout << mr << " + j" << mc << "\n"; }
+private:
+  float mr;
+  float mc;
 };
 
 class Polar {
 public:
   Polar(float mgn=0.0, float ang=0.0) : mg(mgn), ag(agn) {}
+  void show() { std::cout << mg << "<" << ag << "\n"; }
   
 private:
   float mg;
