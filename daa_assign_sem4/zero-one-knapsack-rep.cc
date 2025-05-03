4d8f244a61a2b2f9ae2ab50ad2bb59fdfcd06caa
B4S1C-Coder <saksham2005mittal@gmail.com>
2025-05-03 11:28:32 +0000

    Zero One Knapsack With Repetition via DP

--- daa_assign_sem4/zero-one-knapsack-rep.cc
+++ daa_assign_sem4/zero-one-knapsack-rep.cc
@@ -43,18 +43,59 @@ void zeroOneKnapsackWithRep(
   }
 }
 
+std::vector<int> getSolutionMap(
+  const std::vector<int>& kVector,
+  const std::vector<int>& weights,
+  const std::vector<int>& profits,
+  int capacity
+)
+{
+  std::vector<int> solution(weights.size(), 0);
+  int remainingCap = capacity;
+
+  // w: W -> 1, each k(w) represents the maximum possible profit for capacity w
+  while (remainingCap > 0) {
+    // For each k(w) find an item i such that k(w) = k(w - wi) + vi -> if found that i => 1 else i => 0
+    for (int i = 0; i < weights.size(); i++) {
+      if (weights[i] <= remainingCap && kVector[remainingCap] == kVector[remainingCap - weights[i]] + profits[i]) {
+        solution[i]++;
+        remainingCap -= weights[i];
+        break;
+      }
+    }
+  }
+
+  return solution;
+}
+
+template<typename T>
+void printVector(const std::vector<T>& _vec) {
+  for (const T& x: _vec) { std::cout << x << " "; }
+  std::cout << "\n--------------\n";
+}
+
 int main() {
   const std::vector<int> w = {2, 3, 4};
   const std::vector<int> p = {3, 4, 5};
   std::vector<int> kVec;
 
   zeroOneKnapsackWithRep(kVec, w, p, 5);
+  std::vector<int> sol = getSolutionMap(kVec, w, p, 5);
 
-  for (const int& x: kVec) {
-    std::cout << x << " ";
-  }
+  std::cout << "ITEMS  : ";
+  printVector(std::vector<int>({0, 1, 2}));
 
-  std::cout << "\n";
+  std::cout << "WEIGHTS: ";
+  printVector<int>(w);
+
+  std::cout << "PROFITS: ";
+  printVector<int>(p);
+
+  std::cout << "SOL    : ";
+  printVector<int>(sol);
+
+  std::cout << "kVector: ";
+  printVector(kVec);
 
   return 0;
 }
